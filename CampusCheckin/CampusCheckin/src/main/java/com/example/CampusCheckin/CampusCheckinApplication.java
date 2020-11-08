dd1cd2ce3457e46c5ce1cefab7c230ba4a4aef90
abarnaks <abarnadevi.ks@gmail.com>
2020-11-07 21:53:18 -0500

    pushing the starter project to repo

---
+++ CampusCheckin/CampusCheckin/src/main/java/com/example/CampusCheckin/CampusCheckinApplication.java
@@ -0,0 +1,13 @@
+package com.example.CampusCheckin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCheckinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusCheckinApplication.class, args);
+	}
+
+}
