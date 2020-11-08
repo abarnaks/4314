89841f9908d9ea6f21efaefc82564b52cb465b24
Kyle Hallman <khallman@my.yorku.ca>
2020-11-07 22:49:13 -0500

    added index html and a test html, updated the campuscheckinapplication.java to remove secuirty page

--- CampusCheckin/CampusCheckin/src/main/java/com/example/CampusCheckin/CampusCheckinApplication.java
+++ CampusCheckin/CampusCheckin/src/main/java/com/example/CampusCheckin/CampusCheckinApplication.java
@@ -2,10 +2,13 @@ package com.example.CampusCheckin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//testing commit - kyle
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CampusCheckinApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CampusCheckinApplication.class, args);
 	}
