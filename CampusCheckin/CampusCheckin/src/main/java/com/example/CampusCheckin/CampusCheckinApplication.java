0ce13567c15ac1503d2399d423858a51967b253b
Kyle Hallman <khallman@my.yorku.ca>
2020-11-07 22:19:16 -0500

    testing commit

--- CampusCheckin/CampusCheckin/src/main/java/com/example/CampusCheckin/CampusCheckinApplication.java
+++ CampusCheckin/CampusCheckin/src/main/java/com/example/CampusCheckin/CampusCheckinApplication.java
@@ -2,7 +2,7 @@ package com.example.CampusCheckin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//testing commit - kyle
 @SpringBootApplication
 public class CampusCheckinApplication {
 
