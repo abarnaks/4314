7c655a51687f839b1359c7a7b491223465f3e9b3
abarnaks <abarnadevi.ks@gmail.com>
2020-11-09 20:36:40 -0500

    finished setting up front end jsp classes for application

--- CampusCheckin/CampusCheckin/src/main/java/campusCheckin/CampusCheckinController.java
+++ CampusCheckin/CampusCheckin/src/main/java/campusCheckin/CampusCheckinController.java
@@ -3,11 +3,24 @@ package campusCheckin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CampusCheckinController {
 
+   @RequestMapping("/")
+   public String index() {
+      return "index";
+   }
+
+   @PostMapping("/hello")
+   public String sayHello(@RequestParam("name") String name, Model model) {
+      model.addAttribute("name", name);
+      return "hello";
+   }
+   
 	@GetMapping({"/greeting"})
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
--- CampusCheckin/CampusCheckin/src/main/java/campusCheckin/NotFoundException.java
+++ CampusCheckin/CampusCheckin/src/main/java/campusCheckin/NotFoundException.java
@@ -2,7 +2,7 @@ package campusCheckin;
 
 public class NotFoundException extends RuntimeException {
 
-	NotFoundException(String action, Long id) {
+	public NotFoundException(String action, Long id) {
         super("Could not find " + action + " " + id);
     }
 }
