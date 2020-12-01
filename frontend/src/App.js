5e8cde4db0af451e7ece7b6fc3698b790352189f
Kyle <khallman97@hotmail.com>
2020-12-01 11:08:43 -0500

    updating frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container">
-          <Link className="navbar-brand" to={"/login"}>Campus Check In</Link>
+          <Link className="navbar-brand" to={"/"}>Campus Check In</Link>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
